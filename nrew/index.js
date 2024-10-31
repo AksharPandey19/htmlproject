aee7d2f236a3eb9a9b16b08de8f5f8e635640bca
Akshar Pandey <akshar.nanu@gmail.com>
2024-10-31 22:51:17 +0530

    Lol

--- nrew/index.js
+++ nrew/index.js
@@ -20,17 +20,17 @@ const songs = [
         artist: 'Yung Logos',
     },
     {
-        path: 'assets/1.mp3',
-        displayName: 'The Charmer\'s Call',
-        cover: 'assets/1.jpg',
-        artist: 'Hanu Dixit',
+        path: 'assets/2.mp3',
+        displayName: 'You Will Never See Me Coming',
+        cover: 'assets/2.jpg',
+        artist: 'NEFFEX',
     },
     {
-        path: 'assets/1.mp3',
-        displayName: 'The Charmer\'s Call',
-        cover: 'assets/1.jpg',
-        artist: 'Hanu Dixit',
-    }
+    path: 'assets/1.mp3',
+    displayName: 'The Charmer\'s Call',
+    cover: 'assets/1.jpg',
+    artist: 'Hanu Dixit',
+}
 ];
 
 let musicIndex = 0;
