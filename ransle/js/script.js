ff6cc14312fa5d62c60d8c47673942ad50fe9c6c
Akshar Pandey <akshar.nanu@gmail.com>
2025-01-04 21:57:43 +0530

    f

--- ransle/js/script.js
+++ ransle/js/script.js
@@ -1,13 +1,13 @@
-const fromText = document.querySelector(".from-text");
-const toText = document.querySelector(".to-text");
-const exchageIcon = document.querySelector(".exchange");
-const selectTag = document.querySelectorAll("select");
-const icons = document.querySelectorAll(".row i");
-const translateBtn = document.querySelector("button");
+const fromText = document.querySelector(".from-text"),
+toText = document.querySelector(".to-text"),
+exchageIcon = document.querySelector(".exchange"),
+selectTag = document.querySelectorAll("select"),
+icons = document.querySelectorAll(".row i");
+translateBtn = document.querySelector("button"),
 
 selectTag.forEach((tag, id) => {
     for (let country_code in countries) {
-        let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "de-DE" ? "selected" : "";
+        let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "hi-IN" ? "selected" : "";
         let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
         tag.insertAdjacentHTML("beforeend", option);
     }
@@ -15,7 +15,7 @@ selectTag.forEach((tag, id) => {
 
 exchageIcon.addEventListener("click", () => {
     let tempText = fromText.value,
-        tempLang = selectTag[0].value;
+    tempLang = selectTag[0].value;
     fromText.value = toText.value;
     toText.value = tempText;
     selectTag[0].value = selectTag[1].value;
@@ -23,22 +23,22 @@ exchageIcon.addEventListener("click", () => {
 });
 
 fromText.addEventListener("keyup", () => {
-    if (!fromText.value) {
+    if(!fromText.value) {
         toText.value = "";
     }
 });
 
 translateBtn.addEventListener("click", () => {
     let text = fromText.value.trim(),
-        translateFrom = selectTag[0].value,
-        translateTo = selectTag[1].value;
-    if (!text) return;
+    translateFrom = selectTag[0].value,
+    translateTo = selectTag[1].value;
+    if(!text) return;
     toText.setAttribute("placeholder", "Translating...");
     let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
     fetch(apiUrl).then(res => res.json()).then(data => {
         toText.value = data.responseData.translatedText;
         data.matches.forEach(data => {
-            if (data.id === 0) {
+            if(data.id === 0) {
                 toText.value = data.translation;
             }
         });
@@ -47,17 +47,17 @@ translateBtn.addEventListener("click", () => {
 });
 
 icons.forEach(icon => {
-    icon.addEventListener("click", ({ target }) => {
-        if (!fromText.value || !toText.value) return;
-        if (target.classList.contains("fa-copy")) {
-            if (target.id == "from") {
+    icon.addEventListener("click", ({target}) => {
+        if(!fromText.value || !toText.value) return;
+        if(target.classList.contains("fa-copy")) {
+            if(target.id == "from") {
                 navigator.clipboard.writeText(fromText.value);
             } else {
                 navigator.clipboard.writeText(toText.value);
             }
         } else {
             let utterance;
-            if (target.id == "from") {
+            if(target.id == "from") {
                 utterance = new SpeechSynthesisUtterance(fromText.value);
                 utterance.lang = selectTag[0].value;
             } else {
